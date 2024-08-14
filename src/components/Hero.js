fce5468e8c92e496a92c17ef91c9aeb170864b87
LewBeavenDev <elu95@hotmail.co.uk>
2024-08-14 14:38:57 +0100

    init commit

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="min-h-screen flex flex-col justify-center items-center bg-blue-950">
+      <h1 className="text-lg">Hello World</h1>
+    </section>
+  );
+};
+
+export default Hero;
